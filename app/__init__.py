8114036c3ef807e00e56989529d5cdfc82ec3bd5
Sajad Zarei <zareisajad@protonmail.com>
2021-12-21 14:47:24 +0330

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'signin'
-login.login_message = """if your are new here please use following link to sign up! otherwise login to your account."""
+login.login_message = """
+  if your are new here please use following link to sign up!
+  otherwise login to your account.
+  """
 login.login_message_category = 'danger'
 
 from app import app, routes
